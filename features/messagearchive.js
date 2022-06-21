01ad4a4fee0e132ad827ca6fdfe120fbcb9f5f5d
bishal0602 <bishalpanta60@gmail.com>
2022-06-21 22:44:25 +0545

    Updated archive to support embeds

--- features/messagearchive.js
+++ features/messagearchive.js
@@ -7,27 +7,28 @@ module.exports = (client) => {
       if (member._emoji.name === "📝") {
         const ArchiveChannel =
           member.message.guild.channels.cache.get("988462615468580928");
-        const colorArray = [
-          "#6A0DAD",
-          "#BBDEFB",
-          "#FF9800",
-          "#E91E63",
-          "#26A69A",
-        ];
-        const randomColor =
-          colorArray[Math.floor(Math.random() * colorArray.length)];
-        const editMessageEmbed = new MessageEmbed()
-          .setColor(randomColor)
-          .setAuthor({
-            name: `${member.message.author.username}#${member.message.author.discriminator}`,
-            iconURL: `https://cdn.discordapp.com/avatars/${member.message.author.id}/${member.message.author.avatar}.png`,
-          })
-          .setDescription(member.message.content)
-          .setTimestamp()
-          .setFooter({
-            text: "Message Archive",
-          });
-        ArchiveChannel.send({ embeds: [editMessageEmbed] });
+        if (message.embeds.length != 0) {
+          const messageEmbed = member.message.embeds[0];
+          Archive.send({ embeds: [messageEmbed] });
+        } else {
+          const colorArray = [
+            "#6A0DAD",
+            "#BBDEFB",
+            "#FF9800",
+            "#E91E63",
+            "#26A69A",
+          ];
+          const randomColor =
+            colorArray[Math.floor(Math.random() * colorArray.length)];
+          const editMessageEmbed = new MessageEmbed()
+            .setColor(randomColor)
+            .setAuthor({
+              name: `${member.message.author.username}#${member.message.author.discriminator}`,
+              iconURL: `https://cdn.discordapp.com/avatars/${member.message.author.id}/${member.message.author.avatar}.png`,
+            })
+            .setDescription(member.message.content);
+          ArchiveChannel.send({ embeds: [editMessageEmbed] });
+        }
       }
     } catch (error) {
       console.log(`Message Archive Error: ${error}`);
