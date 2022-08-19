627fbde409ea762abd3addf844b96cc824ed4a21
bishal0602 <bishalpanta60@gmail.com>
2022-08-19 17:47:00 +0545

    vote trigger added for admins

--- features/vote.js
+++ features/vote.js
@@ -1,8 +1,16 @@
 module.exports = (client) => {
   client.on("messageCreate", async (message) => {
+    const roles = ["968674913680506929", "968674913693102120"];
+    let flag = 0;
     try {
       if (!message.content.toLowerCase().includes("vote y")) return;
-      if (!message.member.roles.cache.has("968674913693102120")) return; // only works for CR role
+      roles.forEach((x) => {
+        if (message.member.roles.cache.has(x)) {
+          flag = 1;
+          return;
+        }
+      });
+      if (!flag) return;
       message.react("🇾");
       message.react("🇳");
     } catch (error) {
